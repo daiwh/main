ae56bcd580e144700f4ab984ec9e05cf302a63cf
daiwenhai <daiwenhai2014@163.com>
2016-11-29 16:51:48 +0800

    修改进入界面的activity Appstart.java 第一次可以成功运行

--- java/daiwenhai/weixin/Appsrart.java
+++ java/daiwenhai/weixin/Appsrart.java
@@ -1,5 +1,6 @@
 package daiwenhai.weixin;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
@@ -8,22 +9,22 @@ import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Toast;
 
-public class Appsrart extends AppCompatActivity {
+public class Appsrart extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.appsrart);
-        requestWindowFeature(Window.FEATURE_NO_TITLE); //去掉标题栏
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE); //去掉标题栏
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);//设置全屏
-        Toast.makeText(getApplicationContext(), "孩子，好好背诵", Toast.LENGTH_LONG).show();
-       // overridePendingTransition(R.anim.hyperspace_in, R.anim.hyperspace_out);
-     /*   new Handler().postDelayed(new Runnable() {
+        setContentView(R.layout.appsrart);
+        Toast.makeText(getApplicationContext(), "欢迎使用此版本的微信", Toast.LENGTH_LONG).show();
+       // overridePendingTransition(R.anim.hyperspace_in, R.anim.hyperspace_out);//第一个参数为第一个activity进入时的动画，第二个人参数为第二个activity退出时的动画
+       new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
                 Intent intent = new Intent(Appsrart.this, Welcome.class);
                 startActivity(intent);
                 Appsrart.this.finish();
             }
-        }, 1000);*/
+        }, 1000);
     }
 }
